7d715702a68ac9f29727f97a635cf3fcdb0bf8b8
lauccode <36818928+lauccode@users.noreply.github.com>
2021-12-14 16:19:03 +0100

    Update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -53,6 +53,8 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
+(setq find-name-arg "-iname")
+
 (use-package lsp-mode
   ;; :hook ((prog-mode . lsp-deferred))
   ;; :commands (lsp lsp-deferred)
