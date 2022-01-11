8b523e640c75208df6c5cdaa44a07634575a8fd9
lauccode <36818928+lauccode@users.noreply.github.com>
2022-01-11 22:46:35 +0100

    Update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -98,24 +98,3 @@
 (tool-bar-mode 1)
 
 (setq evil-escape-key-sequence "jj")
-
-;; Using the advising methods above will still create a new buffer if you invoke ^ (dired-up-directory).
-;; To prevent this:
-(eval-after-load "dired"
-  ;; don't remove `other-window', the caller expects it to be there
-  '(defun dired-up-directory (&optional other-window)
-     "Run Dired on parent directory of current directory."
-     (interactive "P")
-     (let* ((dir (dired-current-directory))
-            (orig (current-buffer))
-            (up (file-name-directory (directory-file-name dir))))
-       (or (dired-goto-file (directory-file-name dir))
-           ;; Only try dired-goto-subdir if buffer has more than one dir.
-           (and (cdr dired-subdir-alist)
-                (dired-goto-subdir up))
-           (progn
-             (kill-buffer orig)
-             (dired up)
-             (dired-goto-file dir))))))
-
-
