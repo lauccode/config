b072185a811f560183544f7d1eb437512ced6120
lauccode <36818928+lauccode@users.noreply.github.com>
2022-06-09 18:32:22 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -294,3 +294,41 @@ lvim.plugins = {
 -- pip install pudb
 -- pip install pygame
 -- python -m pudb your_script.py
+	
+-- COLOR FOR LUALINE
+-- local colors = {
+--   color2 = "#0f1419",
+--   -- color3 = "#ffee99",
+--   color3 = "green",
+--   color4 = "#e6e1cf",
+--   color5 = "#14191f",
+--   -- color13 = "#b8cc52",
+--   color13 = "red",
+--   color10 = "#36a3d9",
+--   color8 = "#f07178",
+--   color9 = "#3e4b59",
+-- }
+-- lvim.builtin.line.options.theme = {
+--   normal = {
+--     c = { fg = colors.color9, bg = colors.color2 },
+--     a = { fg = colors.color2, bg = colors.color10, gui = "bold" },
+--     b = { fg = colors.color4, bg = colors.color5 },
+--   },
+--   insert = {
+--     a = { fg = colors.color2, bg = colors.color13, gui = "bold" },
+--     b = { fg = colors.color4, bg = colors.color5 },
+--   },
+--   visual = {
+--     a = { fg = colors.color2, bg = colors.color3, gui = "bold" },
+--     b = { fg = colors.color4, bg = colors.color5 },
+--   },
+--   replace = {
+--     a = { fg = colors.color2, bg = colors.color8, gui = "bold" },
+--     b = { fg = colors.color4, bg = colors.color5 },
+--   },
+--   inactive = {
+--     c = { fg = colors.color4, bg = colors.color2 },
+--     a = { fg = colors.color4, bg = colors.color5, gui = "bold" },
+--     b = { fg = colors.color4, bg = colors.color5 },
+--   },
+-- }
