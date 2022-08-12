3bda07fa6fcf88aff50a05754f3aca3d2396a22a
lauccode <36818928+lauccode@users.noreply.github.com>
2022-08-12 14:19:16 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -279,7 +279,39 @@ lvim.plugins = {
           -- vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
           vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
       end,
-  }
+  },
+     {
+         "f-person/git-blame.nvim",
+         event = "BufRead",
+         config = function()
+             vim.cmd "highlight default link gitblame SpecialComment"
+             vim.g.gitblame_enabled = 1
+         end,
+     },
+     {  -- color for non lsp colorscheme
+         "folke/lsp-colors.nvim",
+         event = "BufRead",
+     },
+     {  -- smooth scrolling
+         "karb94/neoscroll.nvim",
+         event = "WinScrolled",
+         config = function()
+             require('neoscroll').setup({
+                 -- All these keys will be mapped to their corresponding default scrolling animation
+                 mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
+                     '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
+                 hide_cursor = true,          -- Hide cursor while scrolling
+                 stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+                 use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
+                 respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+                 cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
+                 easing_function = nil,        -- Default easing function
+                 pre_hook = nil,              -- Function to run before the scrolling animation starts
+                 post_hook = nil,              -- Function to run after the scrolling animation ends
+             })
+         end
+     },
+     {"simrat39/symbols-outline.nvim"}  -- symbol list on the right !
 }
 	
   lvim.builtin.which_key.mappings["S"]= {
