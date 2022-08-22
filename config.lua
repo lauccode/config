041273c9f1b70f984540bde9a6c9c7be7d4261b4
lauccode <36818928+lauccode@users.noreply.github.com>
2022-08-22 17:02:28 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -311,7 +311,9 @@ lvim.plugins = {
              })
          end
      },
-     {"simrat39/symbols-outline.nvim"}  -- symbol list on the right !
+     {"simrat39/symbols-outline.nvim"},  -- symbol list on the right !
+     {"nvim-treesitter/nvim-treesitter"},
+     {"nvim-orgmode/orgmode"}
 }
 	
   lvim.builtin.which_key.mappings["S"]= {
@@ -370,3 +372,21 @@ lvim.plugins = {
 --     b = { fg = colors.color4, bg = colors.color5 },
 --   },
 -- }
+
+-- ORG MODE
+-- init.lua
+-- Load custom tree-sitter grammar for org filetype
+require('orgmode').setup_ts_grammar()
+-- Tree-sitter configuration
+require'nvim-treesitter.configs'.setup {
+  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = {'org'}, -- Required for spellcheck, some LaTex highlights and code block highlights that do not have ts grammar
+  },
+  ensure_installed = {'org'}, -- Or run :TSUpdate org
+}
+require('orgmode').setup({
+  org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
+  org_default_notes_file = '~/Dropbox/org/refile.org',
+})
