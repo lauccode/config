fd549c29ecc37cc6105dfdf967b6e1d95124fa55
lauccode <36818928+lauccode@users.noreply.github.com>
2022-09-29 09:42:44 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -494,8 +494,8 @@ require("dapui").setup()
 
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)                                                            
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)                                                          
-vim.keymap.set('n', '<F12>', function() require('dap').step_into() end)                                                          
---vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)                                                           
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)                                                          
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)                                                           
 vim.keymap.set('n', '<F9>', function() require('dap').toggle_breakpoint() end)                                                   
 vim.keymap.set('n', '<F6>', function() require('dap').run_last() end)                                                            
 vim.keymap.set('n', '<F7>', function() require('dap').close() end) -- :DapTerminate                                                              
