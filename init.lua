aa2a3791f1459a30207b8ef24ed8439f9e11e4d5
lauccode <36818928+lauccode@users.noreply.github.com>
2023-01-12 21:03:22 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -447,35 +447,6 @@ local config = {
         configurations = {
             cpp = {
                 {
-                    name = "Launch file",
-                    type = "cppdbg",
-                    request = "launch",
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                    cwd = '${workspaceFolder}',
-                    stopAtEntry = false,
-                    setupCommands = {
-                        {
-                            text = '-enable-pretty-printing',
-                            description = 'enable pretty printing',
-                            ignoreFailures = true
-                        },
-                    },
-                },
-                {
-                    name = 'Attach to gdbserver :1234',
-                    type = 'cppdbg',
-                    request = 'launch',
-                    MIMode = 'gdb',
-                    miDebuggerServerAddress = 'localhost:1234',
-                    miDebuggerPath = '/usr/bin/gdb',
-                    cwd = '${workspaceFolder}',
-                    program = function()
-                        return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-                    end,
-                },
-                {
                     name = "Debug UT debian",
                     type = "cppdbg",
                     request = "launch",
@@ -520,7 +491,6 @@ local config = {
                         }
                     }
                 }
-
             },
         },
     },
