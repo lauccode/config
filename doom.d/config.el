2ba8f73f312296ac13f81499dbe22e7e75d15eb1
lauccode <36818928+lauccode@users.noreply.github.com>
2023-02-03 20:52:25 +0100

    Update config.el

--- doom.d/config.el
+++ doom.d/config.el
@@ -363,3 +363,9 @@
    :v "V" #'er/contract-region))
 
 ;; next page of which-key => <C-h>  (all key in vertico)
+
+(map! :n  "C-a"   #'evil-numbers/inc-at-pt
+      :v  "C-a"   #'evil-numbers/inc-at-pt-incremental
+      :v  "C-S-a" #'evil-numbers/inc-at-pt
+      :n  "C-x"   #'evil-numbers/dec-at-pt
+      :v  "C-x"   #'evil-numbers/dec-at-pt-incremental)
