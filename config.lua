85d19b81e5cf0a46fb5611c73227bf20f115fec4
lauccode <36818928+lauccode@users.noreply.github.com>
2023-07-20 15:41:44 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -300,6 +300,49 @@ lvim.plugins = {
     { 'rockyzhang24/arctic.nvim' },
     { 'justinsgithub/oh-my-monokai.nvim'},
     { 'rktjmp/lush.nvim' }, -- used by bluloco and arctic
+    { 'folke/flash.nvim', -- try to replace hop
+        event = "VeryLazy",
+        ---@type Flash.Config
+        opts = {},
+        keys = {
+            {
+                "S",
+                mode = { "n", "x", "o" },
+                function()
+                    -- default options: exact mode, multi window, all directions, with a backdrop
+                    require("flash").jump()
+                end,
+                desc = "Flash",
+            },
+            -- {
+            --     "S",
+            --     mode = { "n", "o", "x" },
+            --     function()
+            --         -- show labeled treesitter nodes around the cursor
+            --         require("flash").treesitter()
+            --     end,
+            --     desc = "Flash Treesitter",
+            -- },
+            {
+                "r",
+                mode = "o",
+                function()
+                    -- jump to a remote location to execute the operator
+                    require("flash").remote()
+                end,
+                desc = "Remote Flash",
+            },
+            {
+                "R",
+                mode = { "n", "o", "x" },
+                function()
+                    -- show labeled treesitter nodes around the search matches
+                    require("flash").treesitter_search()
+                end,
+                desc = "Treesitter Search",
+            }
+        },
+    }, -- TBC
     -- FOR LITEE (call-tree) (TBT)
     -- configure the litee.nvim library
     -- config = function()
