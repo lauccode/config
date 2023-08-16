dad4cbebf35d01cd89881943258e26fdad951f89
lauccode <36818928+lauccode@users.noreply.github.com>
2023-08-16 16:32:02 +0200

    Update custom.el key binding windows for god-mode

--- custom.el
+++ custom.el
@@ -141,6 +141,12 @@
 
 (add-hook 'post-command-hook 'tsa/reflect-god-mode)
 
+;; key binding for god-mode
+(global-set-key (kbd "C-x C-1") #'delete-other-windows)
+(global-set-key (kbd "C-x C-2") #'split-window-below)
+(global-set-key (kbd "C-x C-3") #'split-window-right)
+(global-set-key (kbd "C-x C-0") #'delete-window)
+
 
 ;;███████ ███    ██ ██████  
 ;;██      ████   ██ ██   ██ 
