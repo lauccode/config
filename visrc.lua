9d7368673f726226a6e517244f453e5b746ec66a
lauccode <36818928+lauccode@users.noreply.github.com>
2024-09-22 17:37:23 +0200

    Update visrc.lua

--- visrc.lua
+++ visrc.lua
@@ -1,11 +1,43 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
 
+-- git clone https://github.com/erf/vis-plug.git
+local plug = require('plugins/vis-plug')
+
+-- configure plugins in an array of tables with git urls and options 
+local plugins = {
+
+	-- load a plugin given a repo (https://github.com/ can be omitted and expects a 'init.lua' file)
+	{ 'erf/vis-cursors' },
+
+	-- first parameter is a shorthand for 'url'
+	{ url = 'erf/vis-cursors' },
+
+	-- specify the lua file to require (or theme to set) and give a ref (commit, branch, tag) to checkout
+	{ 'erf/vis-test', file = 'test', ref = 'some-branch' },
+
+	-- specify an alias to later use to access plugin variables (see example below)
+	{ 'erf/vis-highlight', alias = 'hi' },
+
+	-- configure themes by setting 'theme = true'. The theme 'file' will be set on INIT
+	{ 'samlwood/vis-gruvbox', theme = true, file = 'gruvbox' },
+}
+
+-- require and optionally install plugins on init
+plug.init(plugins, true)
+
+-- access plugins via alias
+plug.plugins.hi.patterns[' +\n'] = { style = 'back:#444444' }
+
+
+-- Mapping configuration example
 vis.events.subscribe(vis.events.INIT, function()
-	-- Your global configuration options
+
 end)
 
-vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+vis.events.subscribe(vis.events.WIN_OPEN, 
+function(win)
 	-- Your per window configuration options e.g.
-	vis:command('set number')
+	-- vis:command('set number')
+    vis:command('set number')
 end)
