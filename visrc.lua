1fb54860a1077e48db8348c5af370840e354dcec
lauccode <36818928+lauccode@users.noreply.github.com>
2024-09-22 16:32:44 +0200

    Add files via upload

---
+++ visrc.lua
@@ -0,0 +1,11 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- Your global configuration options
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win) -- luacheck: no unused args
+	-- Your per window configuration options e.g.
+	vis:command('set number')
+end)
