a17f0a1bd135bb6e0aff4c526e4af21ebccc9c53
lauccode <36818928+lauccode@users.noreply.github.com>
2024-10-12 18:10:52 +0200

    Update options.lua

--- lazyvim/options.lua
+++ lazyvim/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- in ~/.config/nvim/lua/config/
 
 vim.g.autoformat = false
 -- Add this to your init.lua or appropriate configuration file :so %
