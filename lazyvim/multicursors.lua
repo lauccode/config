23fc7bde1738bc65f03f712b95c00b1c28b2e514
lauccode <36818928+lauccode@users.noreply.github.com>
2024-10-12 18:12:05 +0200

    Create multicursors.lua

---
+++ lazyvim/multicursors.lua
@@ -0,0 +1,23 @@
+-- in ~/.config/nvim/lua/plugins/
+-- ▗▄▄▖ ▗▖   ▗▖ ▗▖ ▗▄▄▖▗▄▄▄▖▗▖  ▗▖ ▗▄▄▖
+-- ▐▌ ▐▌▐▌   ▐▌ ▐▌▐▌     █  ▐▛▚▖▐▌▐▌   
+-- ▐▛▀▘ ▐▌   ▐▌ ▐▌▐▌▝▜▌  █  ▐▌ ▝▜▌ ▝▀▚▖
+-- ▐▌   ▐▙▄▄▖▝▚▄▞▘▝▚▄▞▘▗▄█▄▖▐▌  ▐▌▗▄▄▞▘
+
+return {
+    "smoka7/multicursors.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        'nvimtools/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+            {
+                mode = { 'v', 'n' },
+                '<Leader>m',
+                '<cmd>MCstart<cr>',
+                desc = 'Multicursors',
+            },
+        },
+}
