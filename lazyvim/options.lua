5375162a7a950323590b3f7593c3321daca869e8
lauccode <36818928+lauccode@users.noreply.github.com>
2024-10-13 14:46:07 +0200

    Update options.lua

--- lazyvim/options.lua
+++ lazyvim/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- SET COLORSCHEME in ~/.config/nvim/init.lua
+-- vim.cmd('colorscheme sorbet')
+
 vim.g.autoformat = false
 -- Add this to your init.lua or appropriate configuration file :so %
 
