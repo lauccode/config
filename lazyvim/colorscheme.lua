f7701af1345077c1de800fcb3124d4de8556734d
lauccode <36818928+lauccode@users.noreply.github.com>
2024-10-17 23:17:31 +0200

    Update colorscheme.lua

--- lazyvim/colorscheme.lua
+++ lazyvim/colorscheme.lua
@@ -1,11 +1,9 @@
 return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-  { "savq/melange-nvim"   },
+  { "savq/melange-nvim"   },  -- set bg=light or dark
   { "NLKNguyen/papercolor-theme" },
   { "overcache/NeoSolarized" },
-
-
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
