3424a386f72abda9833d3b9883c84cbf09b0ebc5
lauccode <36818928+lauccode@users.noreply.github.com>
2025-02-02 11:30:45 +0100

    Update init.el

--- init.el
+++ init.el
@@ -80,13 +80,93 @@
 (global-set-key (kbd "C-c C-M-SPC") 'unhighlight-all-symbols-in-all-buffers)
 
 ;; GOD MODE
+;; GOD MODE
 (use-package god-mode
   :ensure t
   :init
   (god-mode)
-  (global-set-key (kbd "<escape>") #'god-mode-all))
-(custom-set-faces
- '(god-mode-lighter ((t (:inherit error)))))
+  (global-set-key (kbd "<escape>") #'god-local-mode)
+;; (global-set-key (kbd "<escape>") #'god-mode-all)
+
+;; ;; (custom-set-faces
+;; ;;  '(god-mode-lighter ((t (:inherit error)))))
+
+;; change cursor in line = no GOD MODE
+(defun my-god-mode-update-cursor-type ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
+(add-hook 'post-command-hook #'my-god-mode-update-cursor-type)
+
+;; ;; change foreground and foreground for GOD-MODE
+;; (defun my-god-mode-update-mode-line ()
+;;   (cond
+;;    (god-local-mode
+;;     (set-face-attribute 'mode-line nil
+;;                         :foreground "black"
+;;                         :background "DarkGoldenrod3")
+;;     (set-face-attribute 'mode-line-inactive nil
+;;                         :foreground "gray"
+;;                         :background "goldenrod4"))
+;;    (t
+;;     (set-face-attribute 'mode-line nil
+;; 			            :foreground "LightGray"
+;; 			            :background "black")
+;;     (set-face-attribute 'mode-line-inactive nil
+;; 			            :foreground "gray"
+;; 			            :background "DimGray"))))
+;; (add-hook 'post-command-hook #'my-god-mode-update-mode-line)
+
+(defun tsa/god-cursor ()
+  (defvar tsa/cursor-bg (face-attribute 'cursor :background))
+
+  (if (or god-local-mode buffer-read-only)
+      (progn
+        (setq cursor-type 'hbar)
+        (set-face-attribute 'cursor nil
+                            ;; :background "#11ff33"))
+                            :background "orange"))
+    (progn
+      (setq cursor-type 'box)
+      (set-face-attribute 'cursor nil
+                          :background tsa/cursor-bg))))
+
+(defun tsa/god-update-mode-line ()
+  (defvar tsa/fg  (face-attribute 'mode-line :foreground))
+  (defvar tsa/bg  (face-attribute 'mode-line :background))
+  (defvar tsa/fgi (face-attribute 'mode-line-inactive :foreground))
+  (defvar tsa/bgi (face-attribute 'mode-line-inactive :background))
+  (cond
+   (god-local-mode
+    (set-face-attribute 'mode-line nil
+                        :foreground "white"
+                        :background "DarkGoldenrod4")
+    (set-face-attribute 'mode-line-inactive nil
+                        :foreground tsa/fgi
+                        :background tsa/bgi))
+   (t
+    (set-face-attribute 'mode-line nil
+                        :foreground tsa/fg
+                        :background tsa/bg)
+    (set-face-attribute 'mode-line-inactive nil
+                        :foreground tsa/fgi
+                        :background tsa/bgi))))
+
+(defun tsa/reflect-god-mode ()
+  "Make visual changes representing whether God is enabled"
+  (interactive "P")
+  (tsa/god-update-mode-line)
+  (tsa/god-cursor))
+
+(add-hook 'post-command-hook 'tsa/reflect-god-mode))
+(setq god-mode-enable-function-key-translation nil)
+(god-mode)
+(add-to-list 'god-exempt-major-modes 'vterm-mode)
+
+;; key binding for god-mode
+(global-set-key (kbd "C-x C-1") #'delete-other-windows)
+(global-set-key (kbd "C-x C-2") #'split-window-below)
+(global-set-key (kbd "C-x C-3") #'split-window-right)
+(global-set-key (kbd "C-x C-0") #'delete-window)
+
 
 (use-package expand-region
   :ensure t
