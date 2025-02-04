f91953e5a20ea848a964788a2ba8bf63a05bcae6
lauccode <36818928+lauccode@users.noreply.github.com>
2025-02-04 18:17:03 +0100

    Update init.el

--- init.el
+++ init.el
@@ -91,8 +91,8 @@
   :ensure t
   :init
   (god-mode)
-  (global-set-key (kbd "<escape>") #'god-local-mode)
-;; (global-set-key (kbd "<escape>") #'god-mode-all)
+  ;; (global-set-key (kbd "<escape>") #'god-local-mode)
+  (global-set-key (kbd "<escape>") #'god-mode-all)
 
 ;; ;; (custom-set-faces
 ;; ;;  '(god-mode-lighter ((t (:inherit error)))))
