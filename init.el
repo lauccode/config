f2cc85956495e928feb938b7cdac1b0d753ce11e
lauccode <36818928+lauccode@users.noreply.github.com>
2025-02-15 13:31:40 +0100

    Update init.el

--- init.el
+++ init.el
@@ -120,7 +120,7 @@
 ;;                    :branch "main"
 ;;                    :files ("*.el"))
 ;;   :config
-;;   (setq copilot-node-executable "/home/coyote/node-v22.14.0-linux-x64/bin/node")
+;;   (setq copilot-node-executable "~/node-v22.14.0-linux-x64/bin/node")
 ;;   (define-key copilot-mode-map (kbd "M-C-<next>") #'copilot-next-completion)
 ;;   (define-key copilot-mode-map (kbd "M-C-<prior>") #'copilot-previous-completion)
 ;;   (define-key copilot-mode-map (kbd "M-C-<right>") #'copilot-accept-completion-by-word)
