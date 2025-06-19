9f255663c06762b731106bb21c1a1d1386103a9d
lauccode <36818928+lauccode@users.noreply.github.com>
2025-06-19 21:22:35 +0200

    Update init_speed.el

--- init_speed.el
+++ init_speed.el
@@ -541,12 +541,25 @@ Reuse the existing grep buffer window if open."
 ; (define-key projectile-mode-map (kbd "C-c p G") 'projectile-ripgrep-find-file-all)
 
 ;; Tree-sitter
+(use-package tree-sitter
+  :straight t
+  :ensure t
+  :hook ((prog-mode . tree-sitter-mode)
+         (tree-sitter-after-on . tree-sitter-hl-mode))
+  :config
+  (add-to-list 'tree-sitter-major-mode-language-alist '(c++-mode . cpp))
+  (add-hook 'find-file-hook
+            (lambda ()
+              (when (file-remote-p (buffer-file-name))
+                (tree-sitter-mode)
+                (tree-sitter-hl-mode)))))
+
 (use-package tree-sitter-langs
-             :straight t
-             :ensure t
-             :after tree-sitter
-             :config
-             (tree-sitter-require 'cpp))
+  :straight t
+  :ensure t
+  :after tree-sitter
+  :config
+  (tree-sitter-require 'cpp))
 
 ;; (use-package treesit-auto
 ;;   :straight t
