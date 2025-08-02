bf39672abda2e0a14b5cdf85b0ce0f695d617b84
coyote <coyote@pop-os.localdomain>
2025-08-02 16:39:51 +0200

    put font dejavu

--- init_speed2.el
+++ init_speed2.el
@@ -732,4 +732,4 @@ Reuse the existing grep buffer window if open."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Fira Mono" :foundry "CTDB" :slant normal :weight regular :height 90 :width normal)))))
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "PfEd" :slant normal :weight regular :height 90 :width normal)))))
