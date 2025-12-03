cf7e6de3dad69842788c6c89e830bf6c6123c1d2
lauccode <36818928+lauccode@users.noreply.github.com>
2025-12-03 16:33:17 +0100

    Create markdown.lua

---
+++ lazyvim/markdown.lua
@@ -0,0 +1,11 @@
+return {
+    "epwalsh/obsidian.nvim", -- or markdown-todo.nvim
+    opts = {
+        workspaces = {
+            {
+                name = "work",
+                path = "~/vaults/work",
+            },
+        },
+    },
+}
