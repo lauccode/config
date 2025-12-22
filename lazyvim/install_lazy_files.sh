63a438912231d5fa86b3ee51d2eeb9112c5071f5
lauccode <36818928+lauccode@users.noreply.github.com>
2025-12-22 14:11:05 +0100

    Update install_lazy_files.sh

--- lazyvim/install_lazy_files.sh
+++ lazyvim/install_lazy_files.sh
@@ -1,3 +1,3 @@
-cp ~/config/lazyvim/colorscheme.lua ~/config/lazyvim/telescope.lua ~/config/lazyvim/multicursors.lua ~/config/lazyvim/conform_set.lua ~/config/lazyvim/mywords.lua ~/config/lazyvim/image.lua ~/config/lazyvim/markdown.lua ~/.config/nvim/lua/plugins/
+cp ~/config/lazyvim/colorscheme.lua ~/config/lazyvim/telescope.lua ~/config/lazyvim/multicursors.lua ~/config/lazyvim/conform_set.lua ~/config/lazyvim/mywords.lua ~/config/lazyvim/image.lua ~/config/lazyvim/markdown.lua ~/config/lazyvim/orgmode.lua ~/.config/nvim/lua/plugins/
 cp ~/config/lazyvim/options.lua ~/.config/nvim/lua/config/
 cp ~/config/lazyvim/lazy.lua ~/.config/nvim/lua/config/
